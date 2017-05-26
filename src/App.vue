07c1700bb4401ea0adecef0dff9a854f15d9b379
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-05-26 14:18:07 -0700

    clock logic is working

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <h1>Hello</h1>
+    <div>
+      <span>{{hours}}:{{min}}</span><br>
+      <input type="text" placeholder="YOUR NAME"/>
+      <h1>Good Morning, </h1>
+    </div>
   </section>
 </template>
 
@@ -9,8 +13,26 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      hours: '',
+      min: '',
     }
+  },
+  methods: {
+    updateClock: function() {
+      let now = new Date()
+      let hours = now.getHours();
+      let min = now.getMinutes();
+      this.hours = hours
+      this.min = min
+    },
+    initClock: function(){
+      this.updateClock();
+      window.setInterval(this.updateClock, 1000);
+    }
+  },
+  created: function(){
+    this.initClock();
   }
 }
 </script>
@@ -24,8 +46,26 @@ export default {
     background-color: black;
     height: 100vh;
     width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   h1{
     color: white;
+  }
+  input{
+    font-size: 50px;
+    width: 300px;
+    border: none;
+    border-bottom: 2px solid white;
+    background: transparent;
+    text-align: center;
+    color: white;
+    outline: none;
+  }
+  span{
+    color: white;
+    font-size: 150px;
+    font-weight: bolder;
   }
 </style>
